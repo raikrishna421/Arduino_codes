3459ad8d1bd699e46d79089f34c0d17f65db79e4
raikrishna421 <raikrishna421@gmail.com>
2026-03-04 23:12:35 +0530

    first

---
+++ tkinter.ino
@@ -0,0 +1,17 @@
+void setup() {
+  Serial.begin(9600);
+ pinMode(13,OUTPUT);
+}
+
+void loop() {
+  if(Serial.available()) {
+    int score = Serial.parseInt();
+    if(score % 2 == 0) {
+     digitalWrite(13,HIGH);
+    }
+    else
+    {
+      digitalWrite(13,LOW);
+    }
+  }
+}
